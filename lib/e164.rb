c5f340cc14062928ebce9fae36003fa39b921258
Cristian Livadaru <cristian@lcx.at>
2014-07-04 19:32:54 +0200

    add new ranges for austria

--- lib/e164.rb
+++ lib/e164.rb
@@ -1,5 +1,5 @@
 module E164
-  
+
   Dir.glob(File.join(File.dirname(__FILE__), 'e164/*.rb')).each {|f| require f }
 
   ValidFormat = /^\+([\d]{1,3})([\d]{1,14})$/
@@ -10,21 +10,21 @@ module E164
   def self.normalize(num)
     parse(num).unshift(DefaultIdentifier).join
   end
-  
+
   def self.parse(num)
     num = e164_clean(num)
     identifier = Identifiers.include?(num.first) ? num.shift : nil
-    
+
     num = (identifier || num.join.start_with?(DefaultCountryCode)) ? join_country_code(num) : num.unshift(DefaultCountryCode)
     num = join_national_destination_code(num)
-    
+
     [num.shift,num.shift,num.join]
   end
-  
+
   def self.e164_clean(num)
     num.scan(/^(?:#{Identifiers.map{|i| Regexp.escape(i) }.join('|')})|[\d]/)
   end
-  
+
   def self.join_country_code(num)
     potentials = (1..3).map {|l| num[0,l].join}
     country_code = potentials.map {|x| CountryCodes[x] ? x : nil}.compact.first
@@ -33,13 +33,13 @@ module E164
       country_code = DefaultCountryCode
       num.unshift(country_code)
     end
-    
+
     [num.shift(country_code.length).join, *num]
   end
-  
+
   def self.join_national_destination_code(num)
     country = CountryCodes[num[0]]
-    
+
     case (destination_codes = country[:national_destination_codes])
     when Integer
       destination_code_length = destination_codes
@@ -48,7 +48,7 @@ module E164
       destination_code = potentials.map {|x| destination_codes[x] ? x : nil}.compact.first
       destination_code_length = (destination_code && destination_code.length) || destination_codes[:default]
     end
-    
+
     [num.shift, num.shift(destination_code_length).join, *num]
   end
 
@@ -61,6 +61,7 @@ module E164
   def self.is_mobile?(num)
     country=E164.parse(num)[0]
     ndc=E164.parse(num)[1]
+    return false if CountryCodes[country][:national_destination_codes][ndc].nil?
     CountryCodes[country][:national_destination_codes][ndc][:is_mobile]||false
   end
 end
